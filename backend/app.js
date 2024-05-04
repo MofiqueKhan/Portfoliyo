440a7375dfd7c0239ade73fbf6151dec7f24941b
MofiqueKhan <khanmofique@gmail.com>
2024-05-04 18:32:53 +0530

    implement enviroment variables serve file for backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const connectDB = require('./db');
-const cors = require('cors');
-const DataModel = require('./datamodel');
+const express = require("express");
+const path = require("path");
+const connectDB = require("./db");
+const cors = require("cors");
+const DataModel = require("./datamodel");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -10,23 +11,21 @@ connectDB();
 app.use(cors());
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.post('/' , (req ,res)=>{
-    res.send('hello welcome ..! this is from server')
-})
-app.post('/api/data', async (req, res) => {
-    try {
-        const { name, email, userText } = req.body;
-        const newData = new DataModel({ name, email, message: userText });
-        await newData.save();
+app.post("/api/data", async (req, res) => {
+  try {
+    const { name, email, userText } = req.body;
+    const newData = new DataModel({ name, email, message: userText });
+    await newData.save();
 
-        res.status(201).json({ message: 'Data saved successfully' });
-    } catch (error) {
-        console.error('Error saving data:', error);
-        res.status(500).json({ error: 'Server error' });
-    }
+    res.status(201).json({ message: "Data saved successfully" });
+  } catch (error) {
+    console.error("Error saving data:", error);
+    res.status(500).json({ error: "Server error" });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
+
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://aryan:annukhan_143@cluster0.tl9roke.mongodb.net/');
-        console.log('MongoDB connected');
+        await mongoose.connect(process.env.MONGO_URL);
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
     }
